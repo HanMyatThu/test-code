a5815a2c194a1bb8305738c666ccc14be0985383
HanMyatThu <hanmyatthu111@gmail.com>
2021-04-30 18:55:52 +0630

    hello

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 //apply middleware
 const app = express();
+const port = process.env.port || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -17,6 +18,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.listen("3000", () => {
+app.listen(port, () => {
   console.log("server is connected at localhost:3000");
 });
